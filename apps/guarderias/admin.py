2241ade3cbec688bb6cc5b96f73b8c840d69edf8
Andres <xxxandresxxx4@gmail.com>
2026-06-02 00:26:36 -0400

    Sprint #3 ver 3

--- apps/guarderias/admin.py
+++ apps/guarderias/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Guarderia
 
-# Register your models here.
+
+@admin.register(Guarderia)
+class GuarderiaAdmin(admin.ModelAdmin):
+    list_display = [
+        "id_guarderia",
+        "nombre",
+        "telefono",
+        "email",
+        "activo",
+        "created_at",
+    ]
+    list_filter = ["activo"]
+    search_fields = ["nombre", "email", "telefono"]
+    readonly_fields = ["created_at", "updated_at"]
+
+    fieldsets = (
+        (
+            "Información general",
+            {"fields": ("nombre", "direccion", "telefono", "email", "logo")},
+        ),
+        ("Estado", {"fields": ("activo",)}),
+        ("Fechas", {"fields": ("created_at", "updated_at"), "classes": ("collapse",)}),
+    )
